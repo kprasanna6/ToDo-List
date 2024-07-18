46f77b9c86a3a1e05962ab0be00caa7a9bc1d4c8
kprasanna6 <107341276+kprasanna6@users.noreply.github.com>
2024-07-18 10:21:22 +0530

    Add files via upload

---
+++ to-do-list/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import "...styles.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>ToDo-List</h1>
+    </div>
+  );
+};
+export default Header;
